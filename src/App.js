05327a653b62c7aae86259571da95db6827acd18
Ajeibi <ajeibipikin@gmail.com>
2023-06-14 13:58:56 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import AllocationForm from './components/AllocationForm';
-
-
 import { AppProvider } from './context/AppContext';
 import ExpenseList from './components/ExpenseList';
 import ExpenseItem from './components/ExpenseItem';
